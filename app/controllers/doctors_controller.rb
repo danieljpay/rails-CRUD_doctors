a9a6d5639f0ad2269b2de830d33e7e5192676454
danieljpay <danielj.pay@hotmail.com>
2022-04-30 13:16:29 -0500

    punto uno funcionando

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,46 @@
+class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def new 
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to doctors_path
+      puts 'El doctor fue guardado'
+    else
+      render 'new'
+      puts 'El doctor no pudo ser guardado'
+    end
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      puts "El doctor fue actualizado"
+      redirect_to doctors_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Doctor.find(params[:id]).destroy
+    puts 'El doctor fue eliminado'
+    redirect_to doctors_path
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :lastname, :age)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'doctors/index', as: 'doctors'
+  get 'doctors/new', as: 'new_doctor'
+  post '/doctors/create', as: 'create_doctor'
+  get 'doctors/:id/edit', to: 'doctors#edit', as: 'edit_doctor'
+  patch 'doctors/:id/update', to: 'doctors#update', as: 'update_doctor'
+  delete 'doctors/:id/destroy', to: 'doctors#destroy', as: 'destroy_doctor'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "doctors#index"
+end
